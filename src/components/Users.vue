01a52ee068d43c56dd814a6375d14b98eaca1188
paneras <panerasakis@gmail.com>
2019-06-04 00:00:28 +0300

    save & cancel functionallity

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="users-table" class="users">
-  <div class="users__item" v-for="user in users" :key="user.id" @click="$emit('edit:user', user)">
-    <div class="users__image"><img v-bind:src="user.photo"/></div>
-    <div class="users__info">
-      <div class="users__name" >{{ user.name }}</div>
-      <div class="users__email">{{ user.email }}</div>
+  <div id="users-table" class="users" >
+    <div class="users__item" v-for="user in users" 
+                             :key="user.id" :class="{ 'users__item--selected' : active_el == user.id }" 
+                              @click="$emit('edit:user', user), $root.$emit('send-user', user) , activate(user.id)">
+      <div class="users__image"><img v-bind:src="user.photo"/></div>
+      <div class="users__info">
+        <div class="users__name" >{{ user.name }}</div>
+        <div class="users__email">{{ user.email }}</div>
+      </div>
     </div>
-  </div>
-    
   </div>
 </template>
 
@@ -16,7 +17,24 @@ export default {
   name: 'Users',
   props: {
     users: Array,
+  },
+  data() {
+    return {
+       active_el:0
+    };
+  },
+  created() {
+      this.$root.$on('send-saved', () => {
+        this.active_el = 0
+      });
+    },
+  methods:{
+    activate(el){
+        this.active_el = el;
+        this.$root.$emit('send-data', el);
+    }
   }
 }
+
 </script>
 
--- src/components/UsersForm.vue
+++ src/components/UsersForm.vue
@@ -3,73 +3,72 @@
     <div class="form">
       <div>
         <label>Name</label>
-         <input type="text" ref="name" :value="user.name">
-         <!-- <input v-model="user.name" type="text" /> -->
+         <input type="text" ref="name" v-model="initUser.name" :disabled="!isEditingAlive">
       </div>
       <div>
         <label>Email</label>
-        <input type="text" ref="email" :value="user.email">
+        <input type="text" ref="email" v-model="initUser.email" :disabled="!isEditingAlive">
       </div>
       <div>
         <label>Phone</label>
-        <input type="text" ref="phone" :value="user.phone">
+        <input type="text" ref="phone" v-model="initUser.phone" :disabled="!isEditingAlive">
       </div>
       <div>
         <label>Address</label>
-        <input type="text" ref="address" :value="user.address">
+        <input type="text" ref="address" v-model="initUser.address" :disabled="!isEditingAlive">
       </div>
       <div>
         <label>Company</label>
-        <input type="text" ref="company" :value="user.company">
+        <input type="text" ref="company" v-model="initUser.company" :disabled="!isEditingAlive">
       </div>
-      <button v-if="isEditing" @click="cancel">Cancel</button>
-      <button @click="save" :disabled="!isEditing" >Save</button>
+      <div class="form__actions">
+        <button class="form__save" @click="save" :disabled="!isEditingAlive" >Save</button>
+        <button class="form__cancel" v-if="isEditingAlive" @click="cancel">Cancel</button>
+      </div>  
     </div>
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'user-form',
-    props: ['user', 'test'],
+    props: ['user', 'isEditing'],
     data() {
-        return {
-            isEditing: false,
-        };
-    },
-    // created(){
-    //   this.cachedUser = Object.assign({}, this.user);
-    //   console.log(this.cachedUser)
-    // },
-    updated() {
-      //console.log(this.user)
-      //console.log(Object.keys(this.cachedUser).length)
-      if(Object.keys(this.user).length !== 0){   
-        this.isEditing = true;
+      return {
+        initUser: {
+          name: null, 
+          email: null, 
+          phone: null,
+          address: null,
+          company: null,
+          id: null,
+          photo: null
+        },
+        isEditingAlive: false,
       }
+    },
+    created() {
+      this.$root.$on('send-data', (data) => {
+        this.isEditingAlive = true
+      });
+      this.$root.$on('send-user', (data) => {
+        Object.assign(this.initUser, data);
+      });
     },
     methods: {
       save() {
-        //const FinalUser = Object.assign(target, source);
-        this.user.name = this.$refs['name'].value;
-        this.user.email = this.$refs['email'].value;
-        this.user.phone = this.$refs['phone'].value;
-        this.user.address = this.$refs['address'].value;
-        this.user.company = this.$refs['company'].value;
-        //this.cachedUser = Object.assign({}, this.user);
+        Object.assign(this.user, this.initUser);
         this.$emit('update:user', this.user.id , this.user);
-        //Object.assign(this.user, {});
-        this.isEditing = false;
+        this.$root.$emit('send-saved');
+        this.isEditingAlive = false;
       },
       cancel() { 
-        //Object.assign(this.user, this.cachedUser);
         this.$refs['name'].value = this.user.name ;
         this.$refs['email'].value = this.user.email;
         this.$refs['phone'].value = this.user.phone;
         this.$refs['address'].value = this.user.address;
         this.$refs['company'].value = this.user.company;
-        
-        //Object.assign(this.userToEdit, this.cachedUser);
       }
     }
   }
